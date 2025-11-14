360b65b78ad43b8150424a7172a3a0462e137f99
andika <andikabrata10@gmail.com>
2025-11-14 16:51:29 +0700

    udapte libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.material3.android)
+    implementation(libs.androidx.material.icons.android)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.livedata.ktx)
